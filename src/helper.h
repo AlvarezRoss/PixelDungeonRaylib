ce4fc2498bb506c7d8f27d474960581d7a6d412e
AlvarezRoss <alvarezoteromartin@gmail.com>
2026-05-17 10:31:53 +0000

    added level struct

--- src/helper.h
+++ src/helper.h
@@ -5,9 +5,11 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <time.h>
 
 #define SPRITELEN 16
 #define SPRITEHEIGHT 16
+#define ENEMIES_IN_LEVEL_ONE 5
 
 
 typedef struct Animation{
@@ -27,22 +29,33 @@ typedef struct Character{
     Animation* idleAnimation;
     Animation* walkingAnimation;
     Animation* attackAnimation;
+    Animation* hurtAnimation;
     int speed;
+    int maxHealth;
     int health;
     bool attacking;
     bool damaged;
     bool walking;
     bool rotated;
     bool playerDetected; // Only use for enemy characters
+    bool takingDamage;
+    bool enemy;
+    bool inAttackRange; // Used to determine whcih enemies are within range
 }Character;
 
-
+typedef struct level{
+    int enemyNumber;
+    Character* player;
+    Character* enemies;
+}level;
 
 void InitGraphics(void);
 void DeinitGraphics(void);
 int InitAnimation(Animation* animation, const char* path, int frameNumber);
 void DeinitAnimation(Animation* animation); // DELETE MAYBE?
-int InitCharacter(Character* character, Animation* idleAnimation, Animation* walkAnimation, Animation* attackAnimation);
+int InitCharacter(Character* character, Animation* idleAnimation, Animation* walkAnimation, Animation* attackAnimation, Animation* hurtAnimation);
 void UpdateCharacterAnimation(Character* character);
 void HandleCharacterRotation(Character* character);
+void TakeDamage(Character* character, int damage);
+void initLevel(level* level, Character* player, Character* enemies);
 #endif
