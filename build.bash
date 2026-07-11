e27c26a8ba34b0589f9d540d90b05c2035e552a7
AlvarezRoss <alvarezoteromartin@gmail.com>
2026-07-11 11:33:36 +0200

    Changed tracking files

--- build.bash
+++ build.bash
@@ -1,1 +1,4 @@
-gcc src/main.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+#!/bin/bash
+mkdir -p build
+
+gcc src/main.c src/helper.c src/enemyBheaviours.c src/helper.c src/map.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
