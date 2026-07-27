0851260ec03e2e2dcb1a0c84525629686474e08e
AlvarezRoss <alvarezoteromartin@gmail.com>
2026-07-27 10:43:49 +0200

    Added tiles enum

--- src/map.h
+++ src/map.h
@@ -15,7 +15,135 @@ typedef struct TileSet
     Rectangle tileMapSize;
 }TileSet;
 
-typedef enum ELEMENT_TYPE
+typedef enum TILES // This is needed for the map editor and the draw and collision logics
+{
+
+    // ROW 1 //
+    WALL_LEFT_1,
+    WALL_FRONT_1,
+    WALL_FRONT_2,
+    WALL_FRONT_3,
+    WALL_FRONT_4,
+    WALL_RIGHT_1,
+    FLOOR_1,
+    FLOOR_2,
+    FLOOR_3,
+    FLOOR_4,
+
+    // ROW 2 //
+
+    WALL_LEFT_2,
+    FLOOR_5,
+    FLOOR_6,
+    FLOOR_7,
+    FLOOR_8,
+    WALL_RIGHT_2,
+    FLOOR_9,
+    FLOOR_10,
+    FLOOR_11,
+    FLOOR_12,
+
+    // ROW 3 // 
+    WALL_LEFT_3,
+    FLOOR_13,
+    FLOOR_14,
+    FLOOR_15,
+    FLOOR_16,
+    WALL_RIGHT_3,
+    FLOOR_17,
+    FLOOR_18,
+    FLOOR_19,
+    FLOOR_20,
+
+    // ROW 4 // 
+    WALL_LEFT_4,
+    FLOOR_21,
+    FLOOR_22,
+    FLOOR_23,
+    FLOOR_24,
+    WALL_RIGHT_4,
+    DOOR_FRONT_1,
+    DOOR_FRONT_2,
+    HATCH,
+    LADDER,
+
+    // ROW 5 //
+    LEFT_CORNER_WALL,
+    BACK_WALL_1,
+    BACK_WALL_2,
+    BACK_WALL_3,
+    BACK_WALL_4,
+    RIGHT_CORNER_WALL,
+    RIGHT_DOOR_1,
+    LEFT_DOOR_1,
+    RIGHT_DOOR_2,
+    STONES_1,
+
+    // ROW 6 // 
+    TOP_LEFT_CORNER_WALL_1,
+    BACK_WALL_5,
+    BACK_WALL_6,
+    TOP_RIGHT_CORNER_WALL_1,
+    TOP_LEFT_CORNER_WALL_2,
+    TOP_RIGHT_CORNER_WALL_2,
+    RIGHT_DOOR_2,
+    LEFT_DOOR_2,
+    RIGHT_DOOR_2,
+    STONES_2,
+
+    // ROW 7 //
+    FLOOR_25,
+    FLOOR_26,
+    FLOOR_27,
+    FLOOR_28,
+    WEB_1,
+    WEB_2,
+    DOOR_FRONT_3,
+    DOOR_FRONT_4,
+    BONES_1,
+    FLOOR_29,
+
+    // ROW 8 //
+    FLOOR_30,
+    FLOOR_31,
+    FLOOR_32,
+    FLOOR_33,
+    BANNER,
+    DECOR_1,
+    DECOR_2,
+    SKULL_1,
+    FLOOR_34,
+    FLOOR_35,
+
+    // ROW 9 //
+    CHEST_1,
+    CHEST_2,
+    CHEST_3,
+    CHEST_4,
+    CHEST_5,
+    CHEST_6,
+    COIN_1,
+    MANA_SMALL,
+    KEY_1,
+    HP_BIG,
+
+    //ROW 10//
+    TORCH_1,
+    TORCH_2,
+    TORCH_3,
+    TORCH_4,
+    TORCH_5,
+    TORCH_6,
+    TORCH_7,
+    MANA_BIG,
+    HP_SMALL,
+    KEY_2,
+
+
+    TOTAL_TILES
+}TILES;
+
+typedef enum ELEMENT_TYPE // This is used just for the test map - Use the Tiles enum to make actual maps
 {
     NONE,
     DOOR_FRONT,
